24ed9529bacb952b52a7c8cfb5445b567158d596
Oluchukwu Oliseh <oluchukwuoliseh@gmail.com>
2026-07-23 13:19:43 +0100

    Profile image upload upgrade

--- src/app/(protected)/profile/components/ProfileHeader.tsx
+++ src/app/(protected)/profile/components/ProfileHeader.tsx
@@ -29,7 +29,7 @@ export default function ProfileHeader({
   if (!file) return;
 
   const fileExt = file.name.split(".").pop();
-  const fileName = `${profile.id}.${fileExt}`;
+  const fileName = `${profile.id}-${Date.now()}.${fileExt}`;
 
   const { error: uploadError } = await supabase.storage
     .from("avatars")
